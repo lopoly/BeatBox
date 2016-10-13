d3d34e6f8a3d1414c845a250f0cc7a4d25014578
lopoly <lopoly@gmail.com>
2016-10-13 17:15:08 +0300

    Initial commit

---
+++ app/src/main/java/ua/lopoly/beatbox/Sound.java
@@ -0,0 +1,26 @@
+package ua.lopoly.beatbox;
+
+/**
+ * Created by lopoly on 12.10.2016.
+ */
+
+public class Sound {
+
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
